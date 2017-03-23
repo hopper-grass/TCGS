969073ca25d60775b9058c5bd570052b0bb7f3af
DaedricMD <dragonborn2649@gmail.com>
2017-03-23 19:55:21 -0400

    added a way to include the gameLoop as well as a test for it

---
+++ gameLoop.h
@@ -0,0 +1,12 @@
+//Matthew Deyr and Michael Lee
+//this file is needed to include gameLoop.cpp
+#include <iostream>
+
+using namespace std;
+
+#ifndef GAMELOOP__H
+#define GAMELOOP__H
+
+void gameLoop();
+
+#endif
