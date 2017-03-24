93219fc8ebe02bae0beb88fe75987d059452001e
DaedricMD <dragonborn2649@gmail.com>
2017-03-23 20:19:10 -0400

    We can now execute some basic commands succesfully!

--- gameLoop.cpp
+++ gameLoop.cpp
@@ -13,13 +13,17 @@ using namespace std;
 void gameLoop(){
 	
 	int turn = 0;
-//	while(){//while condition?
+	string command = "";
+	string planet1 = "";
+	string planet2 = "";
+	int size = 0;
+
+	while(cin){//is this a decent while condition?
 
 		//here's where the fun begins
-		string command = "";
-		cout << "Enter a command (type 'help' for help)\n";
-		getline(cin, command);
-		
+		cout << "Enter a command (type 'help' for more information):\n";
+		cin >> command;
+
 		if(command == "help"){
 			//print out help menu
 			cout << "The following are valid commands:\n";
@@ -27,10 +31,24 @@ void gameLoop(){
 			cout << "\t\t-> example: attack A C 50\n";
 			cout << "\t- move [from this planet] [to this planet] [size of army]\n";
 			cout << "\t\t-> example: move A B 25\n";
+			cout << "\t- end\n";
+			cout << "\t\t-> used to end your turn\n";
+			cout << "\t- quit\n";
+			cout << "\t\t-> quit the game\n";
+		}else if(command == "quit"){
+			cout << "Thanks for playing!\n";
+			break;
+		}else if(command == "end"){
+			//end the turn?
+			cout << "Turn Ended\n";
+		}else{ //read in the rest
+			cin >> planet1;
+			cin >> planet2;
+			cin >> size;
 		}	
 		
 		//change the player and increment turn
 		++turn;
-//	}
+	}
 
 }
