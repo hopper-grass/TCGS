6f9446175d02772e96eee49d01fc10a2f1c9cdfc
Michael Lee <michael.lee.2pi@gmail.com>
2017-03-27 16:00:01 -0400

    Fixed compile error

--- gameLoop.h
+++ gameLoop.h
@@ -1,12 +1,14 @@
 //Matthew Deyr and Michael Lee
 //this file is needed to include gameLoop.cpp
 #include <iostream>
+#include "Library/player.h"
+#include "Library/Planet.h"
 
 using namespace std;
 
 #ifndef GAMELOOP__H
 #define GAMELOOP__H
 
-void gameLoop();
+void gameLoop(queue<Player*> players, vector<Planet*> planets);
 
 #endif
