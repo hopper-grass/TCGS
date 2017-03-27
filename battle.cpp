6e0f719654330eb6bd51aa6be580b83b530ae81b
Jinjubei <Jinjubei@gmail.com>
2017-03-27 14:57:12 -0400

    Removed a swap file and fixed indents in battle.cpp

--- battle.cpp
+++ battle.cpp
@@ -5,7 +5,7 @@ void seed(){
 void battle(Army* attacker, Army* defender){
   while(attacker->size() > 0 && defender->size() > 0){
     vector<int> aRolls;
-	 vector<int> dRolls;
+    vector<int> dRolls;
     int units,aLoss=0,bLoss=0;
 
     for(int i = 0; i < attacker->size();++i){
@@ -15,11 +15,11 @@ void battle(Army* attacker, Army* defender){
       dRolls.push_back(rollDie());
     }
 
-  		sort(aRolls.begin(), aRolls.end());
-		reverse(aRolls.begin(),aRolls.end());
+    sort(aRolls.begin(), aRolls.end());
+    reverse(aRolls.begin(),aRolls.end());
 
-  		sort(dRolls.begin(),dRolls.end());
-		reverse(dRolls.begin(),dRolls.end());
+    sort(dRolls.begin(),dRolls.end());
+    reverse(dRolls.begin(),dRolls.end());
 
     if(aRolls.size() < dRolls.size()){
       units = aRolls.size();
@@ -34,7 +34,7 @@ void battle(Army* attacker, Army* defender){
         aLoss++;
       }
     }
-    
+
     if(attacker->size() - aLoss <= 0){//if attacker loses more than it has then delete it
       attacker->reinforce(0);
       if(defender->size() - bLoss <= 0){ //If both are reduced to 0 leave defender with 1 unit
@@ -48,9 +48,9 @@ void battle(Army* attacker, Army* defender){
     if(defender->size() - bLoss <= 0){//if defender loses more than it has then delete it
       delete defender;
     }else{
-		defender->eliminate(bLoss);//otherwise reduce by loss
+      defender->eliminate(bLoss);//otherwise reduce by loss
     }
-    
+
   }
 }
 
